7fdc97d2c0c3c443650b853d90f5807ba42f8909
summaiya-sikandar <summaiyaasikandar@gmail.com>
2023-08-19 17:01:50 +0500

    final

---
+++ Firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpO6V3agSvMe3IUT_Pvtd_8HV4q-R0c8A",
+    authDomain: "saylanishoppingstore.firebaseapp.com",
+    projectId: "saylanishoppingstore",
+    storageBucket: "saylanishoppingstore.appspot.com",
+    messagingSenderId: "688654344247",
+    appId: "1:688654344247:web:45a4e210075a5aee9a5fb2",
+    measurementId: "G-65Q97G3FLY"
+  };
+  
